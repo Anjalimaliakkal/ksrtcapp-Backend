a2caf80baf122845de7c98736020b3f7d4506ae5
Anjalimaliakkal <anjalimsanjali2@gmail.com>
2024-06-13 20:10:58 +0530

    signin added

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const cors = require("cors")
 const app=(cors())
 app.arguments(express.json())
 mongoose.connect("mongodb+srv://anjali2003:anjali2003@cluster0.wy6js.mongodb.net/ksrtcdb?retryWrites=true&w=majority&appName=Cluster0")
-
 app.post("/signUp", async (req, res) => {
+
     let input = req.body
     let hashedpassword = await generateHashedPassword(input.password)
     console.log(hashedpassword)
