5f1d38e9c9615ce494e794417552e610b4dc9cf2
Anjalimaliakkal <anjalimsanjali2@gmail.com>
2024-06-13 22:28:05 +0530

    addbus created

--- models/ksrtc.js
+++ models/ksrtc.js
@@ -3,7 +3,11 @@ const schema = mongoose.Schema(
     {
         "name": { type: String, required: true },
         "emailid": { type: String, required: true },
-        "password": { type: String, required: true }
+        "password": { type: String, required: true },
+        "busname": { type: String, required: true },
+            "route": { type: String, required: true },
+            "busnumber": { type: String, required: true },
+            "drivername": { type: String, required: true }
     }
 )
 let ksrtcsmodel = mongoose.model("users", schema);
